7a5f35dc909c04a5da0befc644cd4bae97401433
AZEEM <azeema224143@gmail.com>
2025-02-17 22:37:46 +0530

    security config

---
+++ src/main/java/com/manuu/phdreport/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.manuu.phdreport.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({ExpiredOtpException.class, InvalidOtpException.class, InvalidEmailOrPasswordException.class,
+    UserAlreadyExistsException.class, UserNotFoundException.class})
+    public ResponseEntity<String> handleOtpExceptions(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred: " + ex.getMessage());
+    }
+}
+
